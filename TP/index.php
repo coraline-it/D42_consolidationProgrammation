6b75e89924508c6b6e88c3cb6c319421f5d8252b
coraline-it <c.chau@it-students.fr>
2022-11-15 16:59:53 +0100

    Ajout Exercice 7 et TP à travailler

---
+++ TP/index.php
@@ -0,0 +1,7 @@
+<?php
+// Faire deux pages
+// 1 -> 3 cartes avec avatar, nom, prénom, trigramme, on click d'une des cards
+// 2 -> affichage des infos de la carte cliquée
+
+// Bien commenter le code de tous les exos d'avant :)
+// On peut créer des class objet mais il faut utiliser des tableaux
